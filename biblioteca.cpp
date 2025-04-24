142952cd35a0dcfd659658660d97938792d71471
Augusto <160799418+augustocaixeta@users.noreply.github.com>
2025-04-24 00:29:54 -0300

    .

---
+++ biblioteca.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+struct Node {
+    int pages;
+    Node *next;
+
+    Node(int value) {
+        this->pages = pages;
+        next = NULL;
+    }
+};
+
+struct List {
+    Node *head;
+
+    List() {
+        head = NULL;
+    }
+
+    void insSort(int pages) {
+        Node *n = new Node(pages);
+        if (head == NULL || pages < head->pages) {
+            n->next = head;
+            head = n;
+        } else {
+            Node *cur = head;
+            while (cur->next != NULL && cur->next->pages <= pages) {
+                cur = cur->next;
+            }
+            n->next = cur->next;
+            cur->next = n;
+        }
+    }
+
+    int booksRead(int pages) {
+        int booksRead = 0;
+        Node *aux = head;
+        while (aux != NULL) {
+            if (pages >= aux->pages) {
+                pages -= aux->pages;
+                booksRead++;
+            }
+            aux = aux->next;
+        }
+        return booksRead;
+    }
+};
+
+int main() {
+    List l;
+
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        int value;
+        scanf("%d", &value);
+        l.insSort(value);
+    }
+
+    int p;
+    scanf("%d", &p);
+
+    int booksRead = l.booksRead(p);
+    printf("%d\n", booksRead);
+}
